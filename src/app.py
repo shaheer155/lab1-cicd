921c5a84aa55367429ace765f78ba30f15eda012
shaheer155 <shaheerakash63@gmail.com>
2026-09-02 09:54:10 -0400

    Complete Lab 1 CI setup

---
+++ src/app.py
@@ -0,0 +1,6 @@
+def add_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    print(add_numbers(10, 20))
---
+++ tests/test_app.py
@@ -0,0 +1,5 @@
+from src.app import add_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
